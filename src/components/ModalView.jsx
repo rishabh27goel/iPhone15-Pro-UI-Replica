a74d5d8a34cf535751c5ec99d085ec96e4cb8e6e
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-08-16 15:07:04 +0530

    model-section-completed

--- src/components/ModalView.jsx
+++ src/components/ModalView.jsx
@@ -1,8 +1,51 @@
-import React from 'react';
+import { Html, OrbitControls, PerspectiveCamera, View } from '@react-three/drei';
+import React, { Suspense } from 'react';
+import Lights from './Lights';
+import IPhone from './iPhone';
+import * as THREE from 'three';
+import Loader from './Loader';
 
-const ModalView = () => {
+const ModalView = ({ index, item, size, groupRef, gsapType, controlRef, setRotationState }) => {
   return (
-    <div>ModalView</div>
+    <View
+      id={gsapType}
+      index={index}
+      className={`w-full h-full absolute ${index === 2 && 'right-[-100%]'}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={1} />
+
+      {/* Default Camera */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      {/* Group of Lights */}
+      <Lights />
+
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.7}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group 
+        ref={groupRef} 
+        name={index === 1 ? 'small' : 'large'}
+        position={[0, 0, 0]}  
+      >
+        <Suspense fallback={<Loader />}>
+          <IPhone 
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            rotation={[(10 * Math.PI) / 180, (220 * Math.PI) / 180, 0]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
   );
 }
 
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,19 +2,20 @@ import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import React, { useEffect, useRef, useState } from 'react';
 import ModalView from './ModalView';
-import { yellowImg } from '../utils';
+import { blueImg } from '../utils';
 import { Canvas } from '@react-three/fiber';
 import { View } from '@react-three/drei';
 import * as THREE from 'three';
 import { models, sizes } from '../constants';
+import { animateWithModelTimeline } from '../constants/animations';
 
 const Model = () => {
-
     const [modelSize, setModelSize] = useState('small');
     const [model, setModel] = useState({
-        title: 'iPhone 15 Pro is Natural Titanium',
-        color: ['#8F8A81', '#FFE789', '#6F6C64'],
-        img: yellowImg
+        id: 2,
+        title: "iPhone 15 Pro in Blue Titanium",
+        color: ["#53596E", "#6395ff", "#21242e"],
+        img: blueImg,
     });
 
     // Camera control
@@ -29,12 +30,29 @@ const Model = () => {
     const [smallModelRotation, setSmallModelRotation] = useState(0);
     const [largeModelRotation, setLargeModelRotation] = useState(0);
 
+    const modelsTimeline = gsap.timeline();
+
     useGSAP(() => {
         gsap.to("#heading", {
             opacity: 1,
             y: 0
         });
     }, []);
+
+    useEffect(() => {
+        if (modelSize === 'large') {
+            animateWithModelTimeline(modelsTimeline, smallModel, smallModelRotation, '#small-view', '#large-view', {
+                translateX: '-100%',
+                duration: 2
+            })
+        }
+        else if (modelSize === 'small') {
+            animateWithModelTimeline(modelsTimeline, largeModel, largeModelRotation, '#large-view', '#small-view', {
+                translateX: '0',
+                duration: 2
+            })
+        }
+    }, [modelSize])
 
     return (
         <section className="common-padding">
@@ -51,7 +69,7 @@ const Model = () => {
                         size={modelSize}
                         groupRef={smallModel}
                         gsapType='small-view'
-                        control={cameraControlSmall}
+                        controlRef={cameraControlSmall}
                         setRotationState={setSmallModelRotation}
                     />
 
@@ -61,7 +79,7 @@ const Model = () => {
                         size={modelSize}
                         groupRef={largeModel}
                         gsapType='large-view'
-                        control={cameraControlLarge}
+                        controlRef={cameraControlLarge}
                         setRotationState={setLargeModelRotation}
                     />
 
@@ -90,7 +108,10 @@ const Model = () => {
                                 <li
                                     key={idx}
                                     className="w-6 h-6 rounded-full mx-2 cursor-pointer"
-                                    style={{ backgroundColor: item.color[0] }}
+                                    style={{
+                                        backgroundColor: item.color[0],
+                                        border: item.id === model.id && '2px solid white'
+                                    }}
                                     onClick={() => setModel(item)}
                                 />
                             ))}
@@ -101,9 +122,9 @@ const Model = () => {
                                     <span
                                         key={label}
                                         className="size-btn"
-                                        style={{ 
-                                            backgroundColor: modelSize === value ? 'white' : 'transparent', 
-                                            color: modelSize === value ? 'black' : 'white' 
+                                        style={{
+                                            backgroundColor: modelSize === value ? 'white' : 'transparent',
+                                            color: modelSize === value ? 'black' : 'white'
                                         }}
                                         onClick={() => setModelSize(value)}
                                     >
